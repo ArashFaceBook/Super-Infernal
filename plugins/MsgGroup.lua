59ee4a2e882294d7dcaf2112a380f94228892233
ArashFaceBook <moein.mardan@yahoo.com>
2016-01-21 14:01:35 -0800

    Update MsgGroup.lua

--- plugins/MsgGroup.lua
+++ plugins/MsgGroup.lua
@@ -2,10 +2,10 @@ local function run(msg, matches)
   if not is_sudo(msg) then -- For Sudoers only !
     return 'It is For Sudo Users'
   end
-	if matches[1] == 'msggroup' and is_sudo(msg) then
+	if matches[1] == 'msggroup' and is_admin(msg) then
 		local response = matches[3]
-		send_large_msg("group#id"..matches[2], response)
-						local receiver = 'group#id'..group_id
+		send_large_msg("chat#id"..matches[2], response)
+						local receiver = 'chat#id'..group_id
 				send_large_msg(receiver, response)
 	end
 	end
