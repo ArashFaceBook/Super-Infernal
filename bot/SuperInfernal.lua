2dde18af788bb801ec091411ff93aaccf37e7040
ArashFaceBook <moein.mardan@yahoo.com>
2016-01-21 15:04:13 -0800

    Update SuperInfernal.lua

--- bot/SuperInfernal.lua
+++ bot/SuperInfernal.lua
@@ -204,7 +204,6 @@ function create_config( )
   -- A simple config with basic plugins and ourselves as privileged user
   config = {
     enabled_plugins = {
-    "onservice",
     "inrealm",
     "ingroup",
     "inpm",
