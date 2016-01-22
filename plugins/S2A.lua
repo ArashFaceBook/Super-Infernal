5c07ae6f7543ef3eb8f8f68d17f4f1fda8f18a61
ArashFaceBook <moein.mardan@yahoo.com>
2016-01-21 17:15:05 -0800

    Update S2A.lua

--- plugins/S2A.lua
+++ plugins/S2A.lua
@@ -1,20 +1,25 @@
 local function run(msg, matches)
+	if matches[1] == 'bradcastt' and is_admin(msg) then
+		local response = matches[3]
+		send_large_msg("chat#id"..matches[2], response)
+	end
 	if matches[1] == 'S2A' then
-	if is_admin(msg) then -- Only sudo !
-	return 'For Admins Only!'
-	local data = load_data(_config.moderation.data)
-	local groups = 'groups'
-	local response = matches[2]
-	for k,v in pairs(data[tostring(groups)]) do
-	chat_id =  v
-	local receiver = 'chat#id'..chat_id
-	send_large_msg(receiver, response)
-     end
-  end
+		if is_sudo(msg) then -- Only sudo !
+			local data = load_data(_config.moderation.data)
+			local groups = 'groups'
+			local response = matches[2]
+			for k,v in pairs(data[tostring(groups)]) do
+				chat_id =  v
+				local receiver = 'chat#id'..chat_id
+				send_large_msg(receiver, response)
+			end
+		end
+	end
 end
 return {
   patterns = {
     "^[!/](S2A) +(.+)$",
+    "^[!/](broadcastt) (%d+) (.*)$"
   },
   run = run
 }
